0f03d1ef63ebad6ebacdd4b57937b57ca43afc31
LGalleguillos <44623233+LGalleguillos@users.noreply.github.com>
2018-11-19 18:55:28 -0300

    Arreglos menores + comentarios!

--- main.java
+++ main.java
@@ -9,24 +9,25 @@ public class main {
 	 */
 	public static void main(String[] args) {
 		
-		System.out.println("Ingresa 2 numeros");
+		System.out.println("Ingresa 2 numeros y una operatoria tipo 1+1");
+		Systemtem.out.println("Esta calculadora puede usar los siguientes operatorias +-/*%");
 		int N1=0;
-		int N2=0;
-		double res=0;
 		String op;
+		int N2=0;
+		double res=0;                             
 		Scanner S = new Scanner (System.in);
-		N1=S.nextInt();
+		N1=S.nextInt();                         //ingreso de parameros 
 		N2=S.nextInt();
 		System.out.println("Ingresa operatoria");
 		op=S.next();
-	    switch (op){
+	    switch (op){                        //operar segun el caso 
 	    case "+": res=N1+N2;
 	    case"-": res=N1-N2;
 	    case "*": res=N1*N2;
 	    case "/": res=N1*N2;
 	    case "%": res=N1%N2;
 	    
-			System.out.println("El resultado que buscas es: ");
+			System.out.println("El resultado que buscas es: ");        //mostrando el resultado  
 			System.out.println(res);	
 		}
 	
